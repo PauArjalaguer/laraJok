dfe64fbefeaf5c1fca0ec808e89f3d08ebb3ef9d
Pau Arjalaguer <pau.arjalaguer@gmail.com>
2025-12-21 07:10:26 +0100

    Update Players.php

--- app/Models/Players.php
+++ app/Models/Players.php
@@ -11,7 +11,7 @@ class Players extends Model
     use HasFactory;
     public static function playersByIdTeam($id_team)
     {
-        $result = Players::distinct("playerName")->select('players.idPlayer', 'playerName')->join("player_match", "player_match.idPlayer", "=", "players.idPlayer")->orderBy('playerName', 'asc')
+        $result = Players::distinct("playerName")->select('players.idPlayer', 'playerName','number')->join("player_match", "player_match.idPlayer", "=", "players.idPlayer")->orderBy('playerName', 'asc')
             ->where('idTeam', $id_team)->whereIn('player_match.idMatch', function ($q) use ($id_team) {
                 $q->from('matches')->select('idMatch')->where('idLocal', '=', $id_team)->orwhere('idVisitor',  $id_team)->toSql();
             })->get();
